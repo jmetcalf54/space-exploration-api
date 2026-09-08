87b3fc963b3152dc636fbf367cd35c5c7feac860
Joe Metcalf <josephmetcalf540@gmail.com>
2026-09-08 14:36:31 -0500

    Add asteroid persistence foundation

--- demo/src/main/java/com/example/demo/model/Asteroid.java
+++ demo/src/main/java/com/example/demo/model/Asteroid.java
@@ -1,7 +1,18 @@
 package com.example.demo.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table (name = "asteroids")
 public class Asteroid extends NearEarthObject {
     private int threatLevel;
+
+    @Id 
+    @GeneratedValue
+    private Long id;
 
     public Asteroid(String officialName, Double distanceFromEarth, int threatLevel){
         super(officialName, distanceFromEarth);
@@ -15,6 +26,10 @@ public class Asteroid extends NearEarthObject {
 
     public int getThreatLevel() {
         return threatLevel;
+    }
+
+    public Long getId() {
+        return id;
     }
 
 }
